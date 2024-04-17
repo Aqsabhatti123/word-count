658668273cfdffec9b6d37e88dc3dbde41aa9e30
aqsabhatti123 <aqsabhatti77@gmail.com>
2024-04-18 03:10:13 +0500

    push word count project on github

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+// Import the 'inquirer' module is a commad line interface for node.js
+import inquirer from "inquirer";
+// Declare a const 'answers' and assign it to the result of inquiere.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word:"
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+// Print the array of words to console
+console.log(words);
+// Print the word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
